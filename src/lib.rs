858d534638587f71076106b8e33d2c89f784ec58
UEDA Akihisa <ueda@kyokko.co.jp>
2024-11-09 20:37:09 +0900

    test: テストコードを追加した

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,32 @@
 mod token;
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::token::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_token() {
+        // トークンのシンボルが正しいかのテスト
+        assert_eq!(TokenWithSymbol::new(Token::Whitespace).symbol(), WHITESPACE);
+        assert_eq!(TokenWithSymbol::new(Token::Comma).symbol(), COMMA);
+        assert_eq!(
+            TokenWithSymbol::new(Token::CloseParen).symbol(),
+            CLOSE_PAREN
+        );
+        assert_eq!(TokenWithSymbol::new(Token::OpenParen).symbol(), OPEN_PAREN);
+        assert_eq!(TokenWithSymbol::new(Token::Range('-')).symbol(), RANGE);
+        assert_eq!(
+            TokenWithSymbol::new(Token::Identifier("abc".to_string())).symbol(),
+            IDENTIFIER
+        );
+
+        // 変換結果のテスト
+        let mut token = TokenWithSymbol::new(Token::Whitespace);
+        assert_eq!(token.change_symbol(COMMA), Ok(()));
+        assert_eq!(token.change_symbol(CLOSE_PAREN), Ok(()));
+        assert_eq!(token.change_symbol(OPEN_PAREN), Ok(()));
+        assert_eq!(token.change_symbol(RANGE), Ok(()));
+        assert_eq!(token.change_symbol(IDENTIFIER), Ok(()));
+        assert!(token.change_symbol('a').is_err());
     }
 }
